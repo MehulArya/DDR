8f37ff51a1d8f1010a18f4ed9a54f8824d3fc4e0
parveen kumar <parveenyadavpy57377@gmail.com>
2025-07-29 19:34:07 +0530

    RBAC

--- ddr_portal/Users/urls.py
+++ ddr_portal/Users/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),  # example
+    # path('dashboard/', views.dashboard, name='dashboard'),  # example
+   path('custom_admin/', views.admin_view, name='admin'),
+path('head/', views.head_view, name='head'),
+path('faculty/', views.faculty_view, name='faculty'),
+    path('role_redirect/', views.role_redirect, name='role_redirect'),
 ]
+
